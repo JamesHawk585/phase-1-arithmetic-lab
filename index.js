1686006a2abf8388d68764692ba1c9b1776abfc0
James Hawk <jph94880@gmail.com>
2023-03-15 21:22:19 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() > 0);
+
+console.log(random);
+
+let num3 = 10;
+let num4 = 6;
+
+let mod = num3 - num4;
+
+console.log(mod);
+
+const myArray = [5, 6, 7, 8, 20];
+const max = Math.max(...myArray);
+console.log(max);
